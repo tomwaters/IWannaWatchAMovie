eeb3043d11bd49ef3bbf0d378e4821a440ea20c9
Tom Waters <github@random-works.co.uk>
2017-05-31 23:16:16 +0100

    Added Now, Soon, Later, All filters Select movie to view details popup

--- public/js/iwwm.js
+++ public/js/iwwm.js
@@ -6,7 +6,32 @@ angular.module('iwwmApp', [])
         }
         return iwwmFactory;
     })
-    .controller('iwwmController', function($scope, iwwmFactory) {
+    .filter('iwwmStart', function() {
+        return function(items, mode) {
+            if(!items) {
+                return;
+            }
+
+            let now = new Date('2017-05-29T20:00:00');//Date.now();
+            let modeSoon = new Date(now);
+            modeSoon.setHours(modeSoon.getHours() + 2);
+
+            return items.filter(function(element){
+                let start = new Date(element.Start);
+                switch(mode) {
+                    case 'now':
+                        return start < now && new Date(element.End) > now;
+                    case 'soon':
+                        return start >= now & start < modeSoon;
+                    case 'later':
+                        return start >= modeSoon;
+                    case 'all': default:
+                        return true;
+                }
+            });
+        }
+    })
+    .controller('iwwmController', function($scope, iwwmFactory, iwwmStartFilter) {
         iwwmFactory.getMovies().then(function(response) {
             $scope.movies = response.data;
         }, function() {
